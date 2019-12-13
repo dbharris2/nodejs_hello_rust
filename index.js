47016e29193ea507dc951af0c0e1710ecd74d407
Devon Harris <dbharris2@gmail.com>
2019-12-12 20:16:17 -0800

    add async example

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var x = require('thread_count');
 console.log(x);
 console.log(x.threadCount());
+
+var async = require('async');
+async.performAsyncTask((err, value) => {
+  if (err) throw err;
+  console.log(value);
+})
